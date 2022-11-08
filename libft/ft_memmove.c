4102d252a1ee31787d55242d363fd3f26e6695e8
Leonardo Maia <leosmaia21@gmail.com>
2022-11-08 03:17:19 +0000

    substr corrigida

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,34 +6,61 @@
 /*   By: ledos-sa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 13:57:04 by ledos-sa          #+#    #+#             */
-/*   Updated: 2022/11/06 20:01:02 by ledos-sa         ###   ########.fr       */
+/*   Updated: 2022/11/08 03:04:47 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stddef.h>
-#include <stdlib.h>
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char		*pdest;
-	char		*psrc;
-	char		*tmp;
-	size_t		i;
+	int	i;
 
-	pdest = (char *)dest;
-	psrc = (char *)src;
-	i = -1;
-	tmp = (char *)malloc(sizeof(char ) * n);
-	if (NULL == tmp)
+	if (!dst || !src)
 		return (NULL);
+	if (dst > src)
+	{
+		i = len - 1;
+		while (i >= 0)
+		{
+			*(char *)(dst + i) = *(char *)(src + i);
+			i--;
+		}
+	}
 	else
 	{
-		while (++i < n)
-			tmp[i] = psrc[i];
-		i = -1;
-		while (++i < n)
-			pdest[i] = tmp[i];
+		i = 0;
+		while (i < (int)len)
+		{
+			*(char *)(dst + i) = *(char *)(src + i);
+			i++;
+		}
 	}
-	free(tmp);
-	return (dest);
+	return (dst);
 }
+
+/* void	*ft_memmove(void *dest, const void *src, size_t n) */
+/* { */
+/* 	char		*pdest; */
+/* 	char		*psrc; */
+/* 	char		*tmp; */
+/* 	size_t		i; */
+
+/* 	pdest = (char *)dest; */
+/* 	psrc = (char *)src; */
+/* 	i = -1; */
+/* 	tmp = (char *)malloc(sizeof(char ) * n); */
+/* 	if (NULL == tmp) */
+/* 		return (NULL); */
+/* 	else */
+/* 	{ */
+/* 		while (++i < n) */
+/* 			tmp[i] = psrc[i]; */
+/* 		i = -1; */
+/* 		while (++i < n) */
+/* 			pdest[i] = tmp[i]; */
+/* 	} */
+/* 	free(tmp); */
+/* 	return (dest); */
+/* } */
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ledos-sa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 22:19:53 by ledos-sa          #+#    #+#             */
-/*   Updated: 2022/11/08 00:00:53 by ledos-sa         ###   ########.fr       */
+/*   Updated: 2022/11/08 03:15:32 by ledos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	p = malloc(sizeof(char) * (len + 1));
 	if (!p)
 		return (NULL);
+	if ((unsigned int)ft_strlen(s) <= start)
+	{
+		p[0] = 0;
+		return (p);
+	}
 	i = start;
 	c = 0;
 	while (s[i])
